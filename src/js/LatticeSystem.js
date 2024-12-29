b86def515ae8e12f308cbeff281954b21fe9b7d3
Stanislav Bashkyrtsev <stanislav.bashkyrtsev@elsci.io>
2024-12-29 11:06:16 +0100

    Class LatticeSystem to combine sides & rotation axes
    Still TBD for cube & tetragonal.

--- src/js/LatticeSystem.js
+++ src/js/LatticeSystem.js
@@ -1,7 +1,31 @@
 import Matrix from "./Matrix.js";
 import Vector from "./Vector.js"
+
+class LatticeSystem {
+  /** @type {Matrix[]} */
+  sides;
+  /** @type {Vector[]} */
+  rotationAxes;
+  /** @type {{x: number, y: number: z: number}} */
+  sideLengths;
+  /**
+   * @param {Matrix[]} sides
+   * @param {{x: number, y: number: z: number}} sideLengths
+   * @param {Vector[]} rotationAxes
+   */
+  constructor(sides, sideLengths, rotationAxes) {
+    this.sides = sides;
+    this.rotationAxes = rotationAxes;
+    this.sideLengths = sideLengths
+  }
+}
+
 const cubic = { sides: { x: 50, y: 50, z: 50 } };
 const tetragonal = { sides: { x: 50, y: 70, z: 50 } };
+
+export const cubicFigure1 = new LatticeSystem([], cubic.sides, [new Vector([1, 1, 1])])
+export const tetragonalFigure1 = new LatticeSystem([], tetragonal.sides, [])
+
 const tetragonalFigure = [
   new Matrix([
     new Vector([0, 0, 0]),
